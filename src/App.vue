dfa2d13acc629692968f2446c400725984260124
Jihyeon Kim <simnalamburt@gmail.com>
2025-03-31 09:19:19 +0900

    Minimalize many things

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang=ts>
+// It's clearly a TypeScript block, but Biome fails to recognize it.
+let num: number = 42
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Hello, world!</h1>
+  <p>num is {{ num }}</p>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+h1, p {
+  font-family: sans-serif;
+  color: #e3e3e3;
 }
 </style>
